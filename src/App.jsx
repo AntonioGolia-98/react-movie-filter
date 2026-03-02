04a2192aa0ac2a7251e35ddad1d37073160bf025
AntonioGolia-98 <antoniog98@icloud.com>
2026-03-02 19:20:52 +0100

    map con lista film

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const arrayFilm = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
 
 function App() {
+  const [film, setFilm] = useState(arrayFilm);
+  const [newFilm, setNewFilm] = useState({
+    title: "",
+    genre: ""
+  });
 
   return (
     <>
+      <section>
+        <h1>
+          Lista Film
+        </h1>
+        <ul>
+          {arrayFilm.map((singleFilm, index) =>
+            <li key={index}>
+              {singleFilm.title} {singleFilm.genre}
+
+            </li>
+          )}
+        </ul>
+      </section>
+
     </>
   )
 }
